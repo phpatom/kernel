64248dd0740efa0599658d77a7378aacd5c4aa1b
Nghokeng daniel <nghokend@gmail.com>
2020-09-23 17:25:27 +0100

    EventDispatcherContract added to event provider

--- src/Events/EventServiceProvider.php
+++ src/Events/EventServiceProvider.php
@@ -5,18 +5,27 @@ namespace Atom\App\Events;
 
 use Atom\App\App;
 use Atom\App\Contracts\ServiceProviderContract;
+use Atom\DI\Exceptions\StorageNotFoundException;
+use Atom\Event\Contracts\EventDispatcherContract;
 use Atom\Event\EventDispatcher;
 use Psr\EventDispatcher\EventDispatcherInterface;
 
 class EventServiceProvider implements ServiceProviderContract
 {
-
+    /**
+     * @param App $app
+     * @throws StorageNotFoundException
+     */
     public function register(App $app)
     {
         $c = $app->container();
         $eventDispatcher = new EventDispatcher();
         $c->singletons()->store(
             EventDispatcherInterface::class,
+            $c->as()->object($eventDispatcher)
+        );
+        $c->singletons()->store(
+            EventDispatcherContract::class,
             $c->as()->object($eventDispatcher)
         );
         $c->singletons()->store(
